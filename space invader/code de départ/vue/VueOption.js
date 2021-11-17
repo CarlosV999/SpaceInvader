aa4cbe3e83b1bc73156dcc16e83255d441ac7005
Eric Pelletier <pelletierric@gmail.com>
2021-11-17 17:03:27 -0500

    modification de VueOption.js et html #7

--- space invader/code de départ/vue/VueOption.js
+++ space invader/code de départ/vue/VueOption.js
@@ -12,19 +12,25 @@ class VueOption
 
         document.getElementsByTagName("body")[0].innerHTML = this.html;
 
+        document.getElementById("id").value = this.modifierOption.id;
         document.getElementById("musique").value = this.modifierOption.musique;
         document.getElementById("effets-sonores").value = this.modifierOption.effets;
-        document.getElementById("pseudo").value = this.modifierOption.pseudo;
+        document.getElementById("nom").value = this.modifierOption.nom;
 
         document.getElementById("formulaire-option").addEventListener("submit", evenement => this.modifier(evenement));
 
     }
 
-    modifier(option)
+    modifier(evenement)
     {
+        evenement.preventDefault();
 
+        let id = document.getElementById("id").value;
+        let musique = document.getElementById("musique").value;
+        let effets = document.getElementById("effets-sonores").value;
+        let nom = document.getElementById("nom").value
 
-
+        this.modifierOption(new Joueur(id, null, nom, null, musique, effets));
     }
 
 }
