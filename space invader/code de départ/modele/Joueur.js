b0d4bcda95ec0b2c86f1ce54d5e4ad9abfd1bcce
Eric Pelletier <pelletierric@gmail.com>
2021-11-17 16:49:54 -0500

    ajout des options musique et effets dans Joueur.js #4

--- space invader/code de départ/modele/Joueur.js
+++ space invader/code de départ/modele/Joueur.js
@@ -1,8 +1,10 @@
 class Joueur{
-    constructor(id, position, nom, score){
+    constructor(id, position, nom, score, musique, effets){
       this.id = id;
       this.position = position;
       this.nom = nom;
       this.score = score;
+      this.musique = musique;
+      this.effets = effets;
     }
   }
