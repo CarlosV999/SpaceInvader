7554e645ce66a5148b4c508082ed71f5b1a79f10
Eric Pelletier <pelletierric@gmail.com>
2021-11-18 14:08:20 -0500

    ajout de code dans JeuDAO #4

---
+++ space invader/code de départ/donnees/JeuDAO.js
@@ -0,0 +1,22 @@
+class JeuDAO
+{
+    constructor()
+    {
+        this.joueur = [{id:0, position:1, nom:LeTueur, score:666, musique:true, effets:true}];
+    }
+
+    ajouter()
+    {
+
+
+
+    }
+
+    modifier(joueur)
+    {
+
+
+        
+    }
+
+}
