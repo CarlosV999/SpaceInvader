74ce784305672ce2913816db91936f1679a797a1
Eric Pelletier <pelletierric@gmail.com>
2021-11-18 13:46:24 -0500

    changement dans option et VueOption #7

--- space invader/code de départ/vue/VueOption.js
+++ space invader/code de départ/vue/VueOption.js
@@ -13,9 +13,12 @@ class VueOption
         document.getElementsByTagName("body")[0].innerHTML = this.html;
 
         document.getElementById("id").value = this.modifierOption.id;
+        document.getElementById("position").value = this.modifierOption.position;
+        document.getElementById("nom").value = this.modifierOption.nom;
+        document.getElementById("score").value = this.modifierOption.score;
         document.getElementById("musique").value = this.modifierOption.musique;
         document.getElementById("effets-sonores").value = this.modifierOption.effets;
-        document.getElementById("nom").value = this.modifierOption.nom;
+        
 
         document.getElementById("formulaire-option").addEventListener("submit", evenement => this.modifier(evenement));
 
@@ -26,11 +29,13 @@ class VueOption
         evenement.preventDefault();
 
         let id = document.getElementById("id").value;
+        let position = document.getElementById("position").value;
+        let nom = document.getElementById("nom").value
+        let score = document.getElementById("score").value
         let musique = document.getElementById("musique").value;
         let effets = document.getElementById("effets-sonores").value;
-        let nom = document.getElementById("nom").value
-
-        this.modifierOption(new Joueur(id, null, nom, null, musique, effets));
+        
+        this.modifierOption(new Joueur(id, position, nom, score, musique, effets));
     }
 
 }
