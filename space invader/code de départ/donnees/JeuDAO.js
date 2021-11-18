fcc9a796d064bce82b8200ba44ce121beb7f95cc
Eric Pelletier <pelletierric@gmail.com>
2021-11-18 14:21:09 -0500

    ajout de lister et modifier dans JeuDAO.js #4

--- space invader/code de départ/donnees/JeuDAO.js
+++ space invader/code de départ/donnees/JeuDAO.js
@@ -2,21 +2,32 @@ class JeuDAO
 {
     constructor()
     {
-        this.joueur = [{id:0, position:1, nom:LeTueur, score:666, musique:true, effets:true}];
+        this.banqueJoueurs = {id:0, position:1, nom:LeTueur, score:666, musique:true, effets:true};
     }
 
-    ajouter()
+    lister()
     {
-
-
+        
+            let joueur = new Joueur(this.banqueJoueurs.id,
+                                    this.banqueJoueurs.position,
+                                    this.banqueJoueurs.nom,
+                                    this.banqueJoueurs.score,
+                                    this.banqueJoueurs.musique,
+                                    this.banqueJoueurs.effets);
+        
+        return joueur;    
 
     }
 
     modifier(joueur)
     {
+        joueur.id = this.banqueJoueurs.id;
+        joueur.position = this.banqueJoueurs.position;
+        joueur.nom = this.banqueJoueurs.nom;
+        joueur.score = this.banqueJoueurs.score;
+        joueur.musique = this.banqueJoueurs.musique;
+        joueur.effets = this.banqueJoueurs.effets;
 
-
-        
     }
 
 }
