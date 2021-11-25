2c5fec5b8d2d717bd28f13c96826d4231f781206
Eric Pelletier <pelletierric@gmail.com>
2021-11-25 16:47:36 -0500

    départ de code pour le jeu avec .css .html et .js #8

--- space invader/code de départ/codeJeu.js
+++ space invader/code de départ/codeJeu.js
@@ -1,6 +1,10 @@
 const grille = document.querySelector('.grille')
+const etatDePartie = document.querySelector('.resultat')
 let emplacementJoueur = 202
-
+let deplacement = 15;
+let direction = 1;
+let idEnvahisseurs
+let seDeplaceDroite = true
 
 for(let i = 0; i < 225; i++)
 {
@@ -10,7 +14,7 @@ for(let i = 0; i < 225; i++)
 
 const miniCarres = Array.from(document.querySelectorAll('.grille div'))
 
-const envahisseur = [
+const envahisseurs = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
@@ -18,14 +22,115 @@ const envahisseur = [
 
 function dessiner() 
 {
-    for(let i = 0; i < envahisseur.length; i++)
+    for(let i = 0; i < envahisseurs.length; i++)
     {
 
-        miniCarres[envahisseur[i]].classList.add('envahisseur')
+        miniCarres[envahisseurs[i]].classList.add('envahisseur')
 
     }
 }
 
 dessiner()
 
+function effacer() 
+{
+    for(let i = 0; i < envahisseurs.length; i++)
+    {
+        miniCarres[envahisseurs[i]].classList.remove('envahisseur')
+    }
+}
+
+
 miniCarres[emplacementJoueur].classList.add('joueur')
+
+
+function deplacerJoueur(e)
+{
+    miniCarres[emplacementJoueur].classList.remove('joueur')
+
+    switch(e.key)
+    {
+        case 'ArrowLeft':
+            if(emplacementJoueur % deplacement != 0)
+                emplacementJoueur -=1
+            break
+        case 'ArrowRight':
+            if(emplacementJoueur % deplacement < deplacement -1)
+                emplacementJoueur +=1
+    }
+
+    miniCarres[emplacementJoueur].classList.add('joueur')
+}
+
+document.addEventListener('keydown', deplacerJoueur)
+
+function deplacementEnvahisseurs()
+{
+    const bordGauche = envahisseurs[0] % deplacement === 0
+    const bordDroit = envahisseurs[envahisseurs.length - 1] % deplacement === deplacement - 1
+    effacer()
+
+    if(bordDroit && seDeplaceDroite)
+    {
+        for(let i = 0; i < envahisseurs.length; i++)
+        {
+            envahisseurs[i] += deplacement +1
+            direction = -1
+            seDeplaceDroite = false
+        }
+    }
+
+    if(bordGauche && !seDeplaceDroite)
+    {
+        for(let i = 0; i < envahisseurs.length; i++)
+        {
+            envahisseurs[i] += deplacement -1
+            direction = 1
+            seDeplaceDroite = true;
+        }
+    }
+    for(let i = 0; i < envahisseurs.length; i++)
+    {
+        envahisseurs[i] += direction
+    }
+    
+    dessiner()
+
+    if(miniCarres[emplacementJoueur].classList.contains('envahisseur', 'joueur'))
+    {
+        etatDePartie.innerHTML = 'Vous Êtes Mort!!!'
+        clearInterval(idEnvahisseurs)
+    }
+  
+    for(let i = 0; i< envahisseurs.length; i++)
+    {
+      if(envahisseurs[i] > miniCarres.length)
+      {
+        etatDePartie.innerHTML = 'PERDU!!!'
+        clearInterval(idEnvahisseurs)
+      }     
+    }
+    
+}
+
+idEnvahisseurs = setInterval(deplacementEnvahisseurs, 50)
+
+function tirer(e)
+{
+    let idTire
+    let emplacementTire = emplacementJoueur
+    function mouvementTir()
+    {
+        miniCarres[emplacementTire].classList.remove('tire')
+        emplacementTire -= deplacement
+        miniCarres[emplacementTire].classList.add('tire')
+    }
+    
+    switch(e.key)
+    {
+        case 'ArrowUp':
+            idTire = setInterval(mouvementTir, 100)
+    }
+}
+
+document.addEventListener('keydown', tirer)
