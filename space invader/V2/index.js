b14f110da44538120e08f281341cea9ebee36e59
Carlos Descamps <carlos.descam@hotmail.fr>
2022-03-12 17:22:45 -0500

    #8 tir de laser fonctionnel

--- space invader/V2/index.js
+++ space invader/V2/index.js
@@ -1,4 +1,5 @@
 const shooter = document.getElementById("player-controlled-shooter")
+const mainPlayArea = document.getElementById("main-play-area")
 
 function moveUp() {
     let topPosition = window.getComputedStyle(shooter).getPropertyValue('top')
@@ -44,5 +45,37 @@ function letShipFly(event) {
         fireLaser()
     }
 }
+window.addEventListener("keydown", letShipFly)  
 
-window.addEventListener("keydown", letShipFly)
+function fireLaser(){
+    let laser = createLaserElement()
+    mainPlayArea.appendChild(laser)
+    let laserSFX = new Audio('audio/Pew_sound_effect_128_kbps.mp3')
+    laserSFX.play()
+    moveLaser(laser)
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('left'))
+    let yPosition = parseInt(window.getComputedStyle(shooter).getPropertyValue('top'))
+    let newLaser = document.createElement ('img')
+    newLaser.src = 'images/laser.png'
+    newLaser.classList.add('laser')
+    newLaser.style.left =`${xPosition}px`
+    newLaser.style.top = `${yPosition -20}px`
+    return newLaser
+}
+
+function moveLaser(laser) {
+    let laserInterval = setInterval(()=> {
+        let xPosition = parseInt(laser.style.left)
+        //console.log(xPosition)
+        if (xPosition === 1492) {
+            laser.remove()
+        }
+
+        else {
+            laser.style.left = `${xPosition + 8}px`
+        }
+    }, 10)
+}
